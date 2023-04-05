700d4df6915f068ad9164ccd6c0dfff88e54573f
Daniel Liem <danielliem49@gmail.com>
2023-04-05 15:35:09 -0700

    more search bar styling

--- frontend/src/components/Search/Search.jsx
+++ frontend/src/components/Search/Search.jsx
@@ -1,12 +1,24 @@
 
 import './Search.css';
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 
 export default function Search() {
-    return(
+
+    const handleSearchSubmit = (e) => {
+        e.preventDefault();
+
+    }
+
+    return (
         <>
-            <input type="text" className='search-bar' required></input>
-            <img className='search-bar-icon' src='https://danielliem49-pathfinder-seeds.s3.us-west-1.amazonaws.com/Pathfinder-seed-images/frontendAssets/search-icon.png'></img>
-            <span className='search-bar-labels'>Search</span>
+            <form onSubmit={handleSearchSubmit}>
+                <input type="text" className='search-bar' required></input>
+                <img className='search-bar-icon' src='https://danielliem49-pathfinder-seeds.s3.us-west-1.amazonaws.com/Pathfinder-seed-images/frontendAssets/search-icon.png'></img>
+                <span className='search-bar-labels'>Search</span>
+                <button className="search-button" type="submit">
+                    <ArrowRightAltIcon />
+                </button>
+            </form>
         </>
     )
 }
