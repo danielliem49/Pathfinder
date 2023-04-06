75aa4954be4a3775901e3139d9dd3ed2cae0e9f9
Daniel Liem <danielliem49@gmail.com>
2023-04-06 14:00:16 -0700

    minor fixes

--- app/models/review.rb
+++ app/models/review.rb
@@ -15,6 +15,8 @@ class Review < ApplicationRecord
     validates_presence_of :user_id, :trail_id, :rating, :description, :date_hiked
     validates :user_id, uniqueness: {scope: :trail_id, message: "You've already reviewed this trail!"}
     validates :rating, inclusion: {in: 1..5,  message: "Rating must be between 1 and 5"}
+    validates :description, presence: {"Description can't be blank"}
+    validates :date_hiked, presence: {"Date can't be blank"}
 
     belongs_to :trail,
         foreign_key: :trail_id,
